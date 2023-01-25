68bff8e23bef15e4960b224a60e606202440ea43
[your <arjitverma.go@gmail.com>
2023-01-25 12:03:49 +0530

    completed lending page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import { Download, Features, SessionWrapper } from "./components";
+import assets from './assets';
+import styles from "./styles/Global";
 
-const App =()=>{
+const App = () => {
   return (
-    <div>
-      <h1>React Native NFT showcase</h1>
-    </div>
+    <>
+      <SessionWrapper 
+        title="You own store of Nifty NFTs. Start selling & Growing"
+        description="Buy, store, collect NFTS, exchange & earn crypto. Join 25+ million people using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner='banner'
+       />
+      <SessionWrapper 
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design"
+        mockupImg={assets.homeCards}
+        reverse
+       />
+
+       <Features />
+
+      <SessionWrapper 
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all user's devices. You can easily get your app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+       />
+      <SessionWrapper 
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+       />
+
+       <Download />
+
+       <div className={`px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04`}>
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}</p>
+        <span className="bold">Ravi Gupta</span>
+       </div>
+    </>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Download from "./Download";
+import Features from "./Features";
+import SessionWrapper from "./SessionWrapper";
+
+export {Download, Features, SessionWrapper}
